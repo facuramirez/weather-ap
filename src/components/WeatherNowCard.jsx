68dca631cf57708b58c80365b859e1e54ee3dcea
Facudev <facundoramirez089@gmail.com>
2025-01-12 06:00:58 -0300

    update

--- src/components/WeatherNowCard.jsx
+++ src/components/WeatherNowCard.jsx
@@ -5,7 +5,7 @@ import { obtenerFechaFormateada } from "../helpers/getDate";
 export const WeatherNowCard = ({ temp, weather, name, country }) => {
   return (
     <div className="flex flex-col gap-y-4 bg-[var(--secondary-bg)] p-6 py-8 rounded-2xl ">
-      <h3 className="text-lg lg:text-xl">Now</h3>
+      <h3 className="text-lg lg:text-xl">Ahora</h3>
       <div className="flex flex-col gap-4 border border-l-0 border-r-0 border-t-0 border-[grey] pb-4">
         <div className="flex flex-row items-center gap-6">
           <span className="text-6xl lg:text-7xl">{temp}C</span>
